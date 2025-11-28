64c2ec30d7b0d305c3ba6ec0ed8397a66c8e644f
radecoder <mohdsahildw@gmail.com>
2025-11-28 10:59:06 +0530

    reverts the color of circle modal of hero carousel

--- src/components/home/CircleModal.jsx
+++ src/components/home/CircleModal.jsx
@@ -14,7 +14,171 @@ export default function CircleModal({ isOpen, onClose }) {
   };
 
   return (
-    <div className="fixed inset-0 bg-black/80 backdrop-blur-xl flex items-center justify-center z-50">
+    // <div className="fixed inset-0 backdrop-blur-lg flex items-center justify-center z-50">
+
+    //   <button
+    //     onClick={onClose}
+    //     className="
+    //   absolute top-8 right-20 cursor-pointer
+    //   text-yellow-300 bg-[#0A2342] hover:bg-blue-900/60
+    //   px-4 py-2 rounded-full shadow-lg
+    //   backdrop-blur-lg transition-all hover:scale-110
+    // "
+    //   >
+    //     Close ✕
+    //   </button>
+
+    //   <div
+    //     className="relative w-[600px] h-[600px] rounded-full
+    //   bg-gradient-to-br from-[#0A2342] via-[#0A2342]/80 to-[#0A2342]/90
+    //   backdrop-blur-3xl border border-yellow-300/20
+    //   shadow-[0_0_90px_rgba(255,215,0,0.45)]
+    //   flex items-center justify-center animate-scaleIn overflow-hidden"
+    //   >
+
+    //     {!selectedCollege && (
+    //       <div className="absolute inset-0 rotate-slow opacity-50 pointer-events-none">
+    //         <div className="w-full h-full rounded-full p-[3px] bg-gradient-to-r from-yellow-900 via-yellow-500 to-yellow-900">
+    //           <div className="w-full h-full rounded-full bg-blue-950/40"></div>
+    //         </div>
+    //       </div>
+    //     )}
+
+    //     {!selectedCollege &&
+    //       Array.from({ length: 14 }).map((_, i) => (
+    //         <div
+    //           key={i}
+    //           className="particle absolute w-2 h-2 rounded-full
+    //         bg-yellow-300 blur-[2px]"
+    //           style={{
+    //             left: `${Math.random() * 85 + 7}%`,
+    //             top: `${Math.random() * 85 + 7}%`,
+    //             animationDelay: `${i * 0.5}s`,
+    //           }}
+    //         ></div>
+    //       ))}
+
+    //     <button
+    //       onClick={onClose}
+    //       className="absolute top-6 right-6
+    //     bg-blue-900/40 hover:bg-blue-900/60 text-yellow-300
+    //     p-2 rounded-full backdrop-blur-lg transition-all shadow-xl hover:scale-110"
+    //     >
+    //       ✕
+    //     </button>
+
+    //     {!selectedCollege && (
+    //       <div className="relative w-full h-full pointer-events-auto">
+    //         {collegeNames.map((col, i) => {
+    //           const angle = (i / collegeNames.length) * 2 * Math.PI;
+    //           const radius = 200;
+    //           const x = radius * Math.cos(angle);
+    //           const y = radius * Math.sin(angle);
+
+    //           return (
+    //             <button
+    //               key={col}
+    //               onClick={() => setSelectedCollege(col)}
+    //               className="absolute w-28 h-28 cursor-pointer
+    //             bg-gradient-to-br from-[#0A2342] to-blue-600
+    //             rounded-full text-yellow-300 font-semibold text-xs shadow-xl
+    //             border border-yellow-300/20
+    //             hover:shadow-[0_0_40px_rgba(255,215,0,0.9)]
+    //             hover:scale-130
+    //             transition-all duration-300 floating"
+    //               style={{
+    //                 left: `calc(50% + ${x}px - 56px)`,
+    //                 top: `calc(50% + ${y}px - 56px)`,
+
+    //               }}
+    //             >
+    //               {col}
+    //             </button>
+    //           );
+    //         })}
+    //       </div>
+    //     )}
+
+    //     {selectedCollege && (
+    //       <div className="flex flex-col items-center w-[90%] mx-auto p-4 text-yellow-200">
+    //         {/* TITLE */}
+    //         <h2
+    //           className="
+    //         text-3xl font-extrabold text-center mb-2
+    //         bg-gradient-to-r from-yellow-300 via-yellow-400 to-yellow-200
+    //         bg-clip-text text-transparent
+    //         drop-shadow-[0_0_18px_rgba(255,215,0,0.6)]
+    //       "
+    //         >
+    //           {selectedCollege}
+    //         </h2>
+
+    //         <button
+    //           onClick={() => setSelectedCollege(null)}
+    //           className="
+    //         text-sm text-yellow-300 hover:text-yellow-100
+    //         mb-4 transition-all hover:tracking-wide
+    //         flex items-center gap-1 cursor-pointer
+    //       "
+    //         >
+    //           <span className="text-lg text-yellow-400">
+    //             <IoMdArrowRoundBack className="text-2xl md:text-3xl text-yellow-400" />
+    //           </span>
+    //           Back
+    //         </button>
+
+    //         <div
+    //           className="
+    //         grid sm:grid-cols-2 md:grid-cols-3 gap-4
+    //         w-full max-h-[290px] overflow-y-auto
+    //         pr-1 custom-scroll p-2
+    //       "
+    //         >
+    //           {[
+    //             ...(colleges[selectedCollege].courses || []),
+    //             ...(colleges[selectedCollege].diploma || []),
+    //           ].map(([id, label]) => (
+    //             <button
+    //               key={id}
+    //               onClick={() => handleCourseClick(id)}
+    //               className="
+    //             group relative rounded-xl p-3 h-24
+    //             flex items-center justify-center text-center
+
+    //             bg-blue-900/50
+    //             border border-yellow-300/15
+    //             backdrop-blur-xl
+
+    //             shadow-[0_0_15px_rgba(255,255,255,0.05)]
+    //             hover:shadow-[0_0_25px_rgba(255,215,0,0.45)]
+    //             hover:scale-[1.05]
+    //             hover:border-yellow-300/30
+
+    //             transition-all duration-300
+    //             text-yellow-300 cursor-pointer
+    //           "
+    //             >
+
+    //               <div
+    //                 className="
+    //               absolute inset-0 rounded-xl
+    //               opacity-0 group-hover:opacity-20
+    //               bg-yellow-300/40
+    //               blur-lg transition-all
+    //             "
+    //               ></div>
+
+    //               <span className="text-sm font-semibold z-10 group-hover:text-yellow-400">
+    //                 {label}
+    //               </span>
+    //             </button>
+    //           ))}
+    //         </div>
+    //       </div>
+    //     )}
+    //   </div>
+    // </div>
+    <div className="fixed inset-0 backdrop-blur-lg flex items-center justify-center z-50">
       {/* OUTSIDE CLOSE BUTTON */}
       <button
         onClick={onClose}
